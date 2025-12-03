1212fe00099c3b8ec168712350d76e5913653036
lucasemanuel03 <leblucas3@gmail.com>
2025-12-03 18:35:44 -0300

    cambio de url backend

--- frontend/utils/backendURL.ts
+++ frontend/utils/backendURL.ts
@@ -1,3 +1,5 @@
-const BACKEND_URL = "http://localhost:8081/api";
+//const BACKEND_URL = "http://localhost:8081/api";
+
+const BACKEND_URL = "https://alquigest.onrender.com/api/";
 
 export default BACKEND_URL;
