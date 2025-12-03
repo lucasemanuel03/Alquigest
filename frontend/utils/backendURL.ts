3d43cefa204f9c6d675bbe18caea3fc16ae6622a
lucasemanuel03 <leblucas3@gmail.com>
2025-12-03 19:18:07 -0300

    fix url

--- frontend/utils/backendURL.ts
+++ frontend/utils/backendURL.ts
@@ -1,5 +1,5 @@
 //const BACKEND_URL = "http://localhost:8081/api";
 
-const BACKEND_URL = "https://alquigest.onrender.com/api/";
+const BACKEND_URL = "https://alquigest.onrender.com/api";
 
 export default BACKEND_URL;
